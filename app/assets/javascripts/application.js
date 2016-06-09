af584613848cb562473bc9a99d7d095325698035
Marin Vlastelica P <marin.vlastelicap@gmail.com>
2016-06-09 22:50:26 +0200

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,34 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree .
+
+var hoverColor = 'rgba(0, 0, 200, 0.3)'
+
+$(document).ready(function () {
+
+    var leaveColor = $(".div-button").css("background-color")
+
+
+        $(".div-button").hover(
+        function () {
+            $(this).animate({backgroundColor: hoverColor }, 500)
+    }, function() {
+            $(this).stop().animate({backgroundColor: leaveColor}, 500)
+        }
+    );
+
+});
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,41 @@
+//Place all the behaviors and hooks related to the matching controller here.
+//All this logic will automatically be available in application.js.
+//You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+function split(element) {
+
+    if(element == null) return
+    var text = element.find("h1").text().trim()
+    text = text.split("")
+    var rep = "";
+    for(var i = 0; i < text; i++) {
+        element.appendChild("AAA")
+    }
+    console.log(element)
+    element.show()
+    return element
+
+
+}
+
+
+
+$(document).ready(function () {
+
+
+    var el = $(".page-notice")
+    console.log("Hello from script")
+    children = split(el)
+
+    if(children.length !== 0)
+    for(var i = 0; i < children.length; i++) {
+
+        //children[i].wobble()
+        console.log(i)
+
+
+    }
+
+
+})
